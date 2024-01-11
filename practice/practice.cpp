395a9491ac77c8c2b1762d66f03fb2f7a14ca7e0
SanjibSarkarU <sanjib.ut@gmail.com>
2024-01-11 15:44:50 -0500

    c++

---
+++ practice/practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std; // 
+
+int main()
+{
+	std::cout << "Hello World!";
+	int x = 0;
+	int y = 3;
+	int z = x + y;
+	std::cout << '\n'; // std:: not required
+	std::cout << z << '\n' << x;
+	return 0;
+
+}
